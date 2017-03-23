8759ade3a5648925c85f9980f94876c2b404fbd5
Bastiaan Bijl <bastiaan@sysunite.com>
2017-03-23 20:58:29 +0100

    Alle maanden compleet

--- maanstanden-html/js/main.js
+++ maanstanden-html/js/main.js
@@ -2,7 +2,20 @@
 
 function start() {
 
-  var arr = ['januari', 'februari'];
+  var arr = [
+    'januari',
+    'februari',
+    'maart',
+    'april',
+    'mei',
+    'juni',
+    'juli', 
+    'augustus',
+    'september',
+    'oktober',
+    'november',
+    'december'
+  ];
 
   for(i = 0; i < arr.length; i++) {
     document.getElementById("red_box").innerHTML += (arr[i] + "<br />");
