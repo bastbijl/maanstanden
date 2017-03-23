a7d73ab32c27118a780aa1a2feaa22618453b25a
Bastiaan Bijl <bastiaan@sysunite.com>
2017-03-23 20:57:23 +0100

    Eerste versie printen maanden array

--- maanstanden-html/js/main.js
+++ maanstanden-html/js/main.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+
+
+function start() {
+
+  var arr = ['januari', 'februari'];
+
+  for(i = 0; i < arr.length; i++) {
+    document.getElementById("red_box").innerHTML += (arr[i] + "<br />");
+  }
+
+
+
+}
